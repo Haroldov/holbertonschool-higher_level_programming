6a835e4e95f6edf9cb03486a6011909238cf8d5b
Haroldo Vélez <hvelez020@gmail.com>
2019-05-03 14:23:59 +0000

    [fix] use add_0 once in my code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    import add_0 as math
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{0:d} + {1:d} = {2:d}".format(a, b, math.add(a, b)))
