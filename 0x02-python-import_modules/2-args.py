83be700a017f5453f89c0cd6738634506110b176
Haroldo Vélez <hvelez020@gmail.com>
2019-05-03 15:04:45 +0000

    [fix] plural and singular arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     argc = len(argv)
-    str = "argument"
+    str = "arguments"
     char = ":"
     if argc == 1:
         char = "."
-    elif argc > 2:
-        str = "arguments"
+    elif argc == 2:
+        str = "argument"
     print("{0:d} {2:s}{1:s}".format(argc - 1, char, str))
     for i, arg in enumerate(argv[1::], start=1):
         print("{0:d}: {1:s}".format(i, arg))
