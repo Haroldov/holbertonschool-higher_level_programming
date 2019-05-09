baeaad21057af9c6a1097080ad97643060d3f50c
Haroldo Vélez <hvelez020@gmail.com>
2019-05-09 23:05:51 +0000

    [add] task #13

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D':500, 'M': 1000}
+    num = 0
+    tmp = 0
+    pastNum = 0
+    for c in roman_string:
+        tmp = dic.get(c, 0)
+        if tmp > pastNum:
+            num = tmp - num
+        else:
+            num += tmp
+        pastNum = tmp
+    return num
