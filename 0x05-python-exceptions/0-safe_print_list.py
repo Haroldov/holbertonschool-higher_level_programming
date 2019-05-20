74708123db832715a5d0111453cfc982cac5a3e8
Haroldo Vélez <hvelez020@gmail.com>
2019-05-20 23:36:14 +0000

    [fix] when input is negative

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        i = -1
         for i in range(x):
             print(my_list[i], end="")
-    except IndexError:
-        i -= 1
-        pass
+    except:
+        if i > 0:
+            i -= 1
+        else:
+            i = -1
     finally:
         print()
         return i + 1
