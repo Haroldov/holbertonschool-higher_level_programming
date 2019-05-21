09d305e9fdd94c514f4a3ced1ca01fe8614fd9a5
Haroldo Vélez <hvelez020@gmail.com>
2019-05-21 00:05:52 +0000

    [fix] returning right value

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = -1
+    count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
         except IndexError:
             raise
         except:
+            count += 1
             pass
     print()
-    return i + 1
+    return i + 1 - count
