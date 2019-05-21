c94df19c5284b23ed17d8cc10200935ce5e46820
Haroldo Vélez <hvelez020@gmail.com>
2019-05-21 00:01:25 +0000

    [

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = -1
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+    print()
+    return i + 1
