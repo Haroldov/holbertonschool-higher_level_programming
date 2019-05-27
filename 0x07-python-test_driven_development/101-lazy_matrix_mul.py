713bfd2eb6f204f2f7718a2a87e9c5ddd5ddbd82
Haroldo Vélez <hvelez020@gmail.com>
2019-05-27 18:00:26 +0000

    [reversing]

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,9 +7,4 @@ module that has matrix operations
 def lazy_matrix_mul(m_a, m_b):
     """ matrix multiplication """
     import numpy as np
-    if len(m_a) == 0 or len(m_a[0]) == 0:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) == 0 or len(m_b[0]) == 0:
-        raise ValueError("m_b can't be empty")
-
     return np.matmul(np.asarray(m_a), np.asarray(m_b))
