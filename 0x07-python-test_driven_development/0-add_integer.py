0500761da930533c3c554bebf36f408dcdde3a03
Haroldo Vélez <hvelez020@gmail.com>
2019-05-27 15:40:00 +0000

    [fix] pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,6 +17,8 @@ def add_integer(a, b=98):
         a = 89
     if b != b:
         b = 89
+    if a is None:
+        raise TypeError("a must be an integer")
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
