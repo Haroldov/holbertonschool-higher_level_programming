77e6fba9e9103eaba034f7781eabf4601261d1b9
Haroldo Vélez <hvelez020@gmail.com>
2019-05-27 19:24:30 +0000

    [fix]

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,4 +7,4 @@ module that has matrix operations
 def lazy_matrix_mul(m_a, m_b):
     """ matrix multiplication """
     import numpy as np
-    return np.matmul(np.asarray(m_a), np.asarray(m_b))
+    return np.matmul(m_a, m_b).tolist()
