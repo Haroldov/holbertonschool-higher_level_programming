3b5dbb1c0b742dea3fa6b42d202e229ab2d8c912
Haroldo Vélez <hvelez020@gmail.com>
2019-05-27 15:38:46 +0000

    [fix] pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,16 +14,16 @@ def add_integer(a, b=98):
     """
 
     if a != a:
-        a = 0
+        a = 89
     if b != b:
-        b = 98
+        b = 89
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
-    if (a + b) == float('inf') or (a +b) == -float('inf'):
-        return 1
+    if (a + b) == float('inf') or (a + b) == -float('inf'):
+        return 89
     if type(a) is float or type(b) is float:
         a = int(a)
         b = int(b)
