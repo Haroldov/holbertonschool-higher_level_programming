5a71e905a5d2614d5a6af582cbf8c89b59ea6c12
Haroldo Vélez <hvelez020@gmail.com>
2019-05-27 19:27:28 +0000

    [fix] returning array instead of matrix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,4 +7,4 @@ module that has matrix operations
 def lazy_matrix_mul(m_a, m_b):
     """ matrix multiplication """
     import numpy as np
-    return np.matmul(m_a, m_b).tolist()
+    return np.matmul(m_a, m_b)
