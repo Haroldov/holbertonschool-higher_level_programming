0180b0247b80195305843098733f025d427408cc
Haroldo Vélez <hvelez020@gmail.com>
2019-06-05 22:00:07 +0000

    [fix] offset of file descriptor to 0

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,6 +2,7 @@
 def read_lines(filename="", nb_lines=0):
     with open(filename) as f:
         if nb_lines <= 0 or nb_lines >= sum([1 for line in f]):
+            f.seek(0)
             print(f.read(), end='')
         else:
             f.seek(0)
