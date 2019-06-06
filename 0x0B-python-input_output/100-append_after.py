632f30a3e0176dcab1e5257f40e564d2171333ee
Haroldo Vélez <hvelez020@gmail.com>
2019-06-06 01:47:25 +0000

    [add] task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,7 +12,8 @@ def append_after(filename="", search_string="", new_string=""):
     count = 1
     for numLine, line in enumerate(lines[:]):
         if search_string in line:
-            lines = lines[:numLine + count] + [new_string[:-1]] + lines[numLine + count:]
+            lines = (lines[:numLine + count] +
+                     [new_string.replace('\n', '')] + lines[numLine + count:])
             count += 1
     new_text = "\n".join(lines) + '\n'
     with open(filename, 'w') as f:
