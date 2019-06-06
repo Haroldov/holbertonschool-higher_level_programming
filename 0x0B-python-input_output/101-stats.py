7c4a95fd887d1632861d4c6ffcd04f41f637e4a9
Haroldo Vélez <hvelez020@gmail.com>
2019-06-06 03:05:01 +0000

    []

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,20 +7,25 @@ try:
         count = 0
         status = {}
         size = 0
+        possStatus = ["200", "301", "400", "401", "403", "404", "405", "500"]
         for line in sys.stdin:
-                count += 1
-                size += int(line.split(' ')[-1][:-1])
                 tmpStatus = line.split(' ')[-2]
                 if tmpStatus in status.keys():
+                        count += 1
+                        size += int(line.split(' ')[-1][:-1])
                         status.update({tmpStatus: status.get(tmpStatus) + 1})
-                else:
+                elif tmpStatus in possStatus:
+                        count += 1
+                        size += int(line.split(' ')[-1][:-1])
                         status.update({tmpStatus: 1})
                 if (count == 10):
                         count = 0
                         print("File size: {}".format(size))
-                        for key, val in status.items():
+                        tmpDic = sorted(status.items(), key=lambda t: t[0])
+                        for key, val in tmpDic:
                                 print("{}: {}".format(key, val))
 except KeyboardInterrupt as e:
         print("File size: {}".format(size))
-        for key, val in status.items():
+        tmpDic = sorted(status.items(), key=lambda t: t[0])
+        for key, val in tmpDic:
                 print("{}: {}".format(key, val))
