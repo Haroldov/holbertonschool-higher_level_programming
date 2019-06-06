2b8d427fc73fde18634c86e4c5e2f56a39bf94aa
Haroldo Vélez <hvelez020@gmail.com>
2019-06-06 00:01:23 +0000

    [add] task #14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    triag = [[1]]
+    for i in range(n - 1):
+        row = [1]
+        if len(triag[i]) == 1:
+            row += [1]
+        else:
+            row += [x + y for x, y in zip(triag[i], [0] + triag[i][:-1])][1:]
+            row += [1]
+        triag.append(row)
+    return triag
