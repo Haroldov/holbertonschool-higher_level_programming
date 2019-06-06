af17a84d97b2a7ec9caacfa3f03ec1ff7aa01f17
Haroldo Vélez <hvelez020@gmail.com>
2019-06-06 02:50:28 +0000

    [add] task 101

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import signal
+import sys
+
+
+try:
+        count = 0
+        status = {}
+        size = 0
+        for line in sys.stdin:
+                count += 1
+                size += int(line.split(' ')[-1][:-1])
+                tmpStatus = line.split(' ')[-2]
+                if tmpStatus in status.keys():
+                        status.update({tmpStatus: status.get(tmpStatus) + 1})
+                else:
+                        status.update({tmpStatus: 1})
+                if (count == 10):
+                        count = 0
+                        print("File size: {}".format(size))
+                        for key, val in status.items():
+                                print("{}: {}".format(key, val))
+except KeyboardInterrupt as e:
+        print(count)
+        print("File size: {}".format(size))
+        for key, val in status.items():
+                print("{}: {}".format(key, val))
+        sys.exit(0)
