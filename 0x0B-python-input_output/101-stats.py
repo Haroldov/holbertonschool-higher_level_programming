7b11cd9e0bc4e8077d82510bb06dcd32ae0b7f3e
Haroldo Vélez <hvelez020@gmail.com>
2019-06-07 00:31:05 +0000

    [fix]

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,7 +14,7 @@ size = 0
 possStatus = ["200", "301", "400", "401", "403", "404", "405", "500"]
 signal.signal(signal.SIGINT, receiveSignal)
 for line in sys.stdin:
-        if len(line.split(' ')) == 8:
+        if len(line.split(' ')) == 9:
                 tmpStatus = line.split(' ')[-2]
                 if tmpStatus in status.keys():
                         count += 1
