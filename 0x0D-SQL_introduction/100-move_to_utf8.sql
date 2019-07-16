5a2df361387572ec10202d4a6c8f61112296237e
Haroldo Vélez <hvelez020@gmail.com>
2019-07-16 03:46:45 +0000

    [fix] task #10

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,4 +5,4 @@
 -- Table first_table
 -- Field name in first_table
 USE hbtn_0c_0;
-ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 COLLATE = utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE first_table MODIFY name varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
