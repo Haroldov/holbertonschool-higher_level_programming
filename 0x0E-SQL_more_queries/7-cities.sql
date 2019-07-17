619662bb4d062149183945032e91b16740c97b1f
Haroldo Vélez <hvelez020@gmail.com>
2019-07-17 02:53:41 +0000

    [add] UNIQUE to id

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,6 +9,6 @@
 -- If the table cities already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-       (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+       (id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
        `state_id` INT NOT NULL, FOREIGN KEY(`state_id`) REFERENCES `states`(`id`),
        name VARCHAR(256) NOT NULL);
