eb604dd91d79feffd29dfd67637b33d988d057cf
Haroldo Vélez <hvelez020@gmail.com>
2019-07-17 05:10:09 +0000

    s

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,6 @@
 -- doc aslkdskdsjdlad
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres
        LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+       WHERE tv_show_genres.show_id IS NOT NULL
        GROUP BY tv_genres.name
        ORDER BY number_of_shows DESC;
