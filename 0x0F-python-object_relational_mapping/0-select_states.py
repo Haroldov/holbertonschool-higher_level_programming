471a1c3867f72cba4af2228d34992a62247c41f2
root <hvelez020@gmail.com>
2019-08-09 21:01:52 +0000

    [fix] pep8 style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,11 @@
 import MySQLdb
 from sys import argv
 
-if len(argv) == 4:
-   db = MySQLdb.connect(user=argv[1], passwd=argv[2], port=3306, host='localhost', db=argv[3])
-   cursor = db.cursor()
-   cursor.execute('SELECT * FROM states ORDER BY id ASC')
-   for row in cursor.fetchall():
-      print(row)
-
+if __name__ == "__main__":
+    if len(argv) == 4:
+        db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                             port=3306, host='localhost', db=argv[3])
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM states ORDER BY id ASC')
+        for row in cursor.fetchall():
+            print(row)
