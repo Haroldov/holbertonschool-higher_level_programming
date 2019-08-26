dcfdd48797453d881f8f6018cc7cbc2ce0d4b4df
Haroldo Vélez <hvelez020@gmail.com>
2019-08-26 21:47:02 +0000

    [fix] __main__ == __name__

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen(sys.argv[1]) as response:
         html = response.info()
-        print(dict(html)['X-Request-Id'])
+        print(dict(html).get('X-Request-Id'))
