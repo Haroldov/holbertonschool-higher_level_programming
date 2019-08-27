8cbc80c6c2bb417919fd2eb4ad16c3b8717db82d
Haroldo Vélez <hvelez020@gmail.com>
2019-08-27 00:27:35 +0000

    [add] task #8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" doc """
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 2:
+        data = {'q': sys.argv[1]}
+    else:
+        data = {'q': ''}
+    res = requests.post(url, data=data)
+    try:
+        des = res.json()
+        if len(des) == 0:
+            print("No result")
+        else:
+            print("{}: {}".format(des.get("id"), des.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
