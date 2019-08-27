2d513e2225416676d801cc77875b9e55f625a5db
Haroldo Vélez <hvelez020@gmail.com>
2019-08-27 03:19:52 +0000

    [change] name to the task

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -20,10 +20,16 @@ if __name__ == '__main__':
             results = des.get("results")
             for json in results:
                 print(json.get("name"))
+                for film in json.get("films"):
+                    name = requests.get(film).json().get("title")
+                    print("\t{}".format(name))
             while (des.get("next") is not None):
                 des = requests.get(des.get("next")).json()
                 for json in des.get("results"):
                     print(json.get("name"))
- 
+                    for film in json.get("films"):
+                        name = requests.get(film).json().get("title")
+                        print("\t{}".format(name))
+
     except ValueError:
         print("Not a valid JSON")
