3b58809d11b8359504f6479c457268b69ea7babd
Haroldo Vélez <hvelez020@gmail.com>
2019-08-27 00:51:00 +0000

    [add] task #9

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" doc """
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "https://swapi.co/api/people/"
+    if len(sys.argv) == 2:
+        data = {'search': sys.argv[1]}
+    else:
+        data = {'search': ''}
+    res = requests.get(url, params=data)
+    try:
+        des = res.json()
+        if len(des) == 0:
+            print("No result")
+        else:
+            print("Number of results: {}".format(des.get("count")))
+            results = des.get("results")
+            for json in results:
+                print("{}".format(json.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
