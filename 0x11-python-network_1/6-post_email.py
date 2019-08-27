b2e976b0436fcb0c6c8ff326069d99e295572f88
Haroldo Vélez <hvelez020@gmail.com>
2019-08-27 00:04:06 +0000

    [fix] change , for : in the definition of a dictionary

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    data = {'email', sys.argv[2]}
-    res = requests.post(url, data)
+    data = {'email': sys.argv[2]}
+    res = requests.post(url, data=data)
     print(res.text)
