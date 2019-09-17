c832b3f08774b644ec75bf19a26dd8250ee32eef
root <hvelez020@gmail.com>
2019-09-17 21:15:19 +0000

    [add] task #6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let str = '';
-for (str of arr) {
-  console.log(str);
+let ind = 0;
+for (; ind < arr.length; ind++) {
+  console.log(arr[ind]);
 }
