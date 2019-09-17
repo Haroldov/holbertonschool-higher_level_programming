a975cbf4db96df6234f24791f5bcc37762d7d62a
root <hvelez020@gmail.com>
2019-09-17 19:34:35 +0000

    [fix] changing js arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 let length = process.argv.length;
 if (length === 2) {
   console.log('No argument');
-} else {
+} else if (length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
