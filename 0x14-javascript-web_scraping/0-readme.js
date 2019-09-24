375a59b1b48413ed53f05b64c1896b7b867cf18f
root <hvelez020@gmail.com>
2019-09-24 17:49:54 +0000

    [fix] semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fd = require('fs');
-cb = (err, data) => {
-    if (err) {
-      console.error(err)
-    } else {
-      process.stdout.write(data)
-    }
-}
-txt = fd.readFile(process.argv[2], 'utf-8', cb)
+const cb = (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    process.stdout.write(data);
+  }
+};
+fd.readFile(process.argv[2], 'utf-8', cb);
