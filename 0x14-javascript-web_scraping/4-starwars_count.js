6457966445353670f6b316b50296c780dd8a0702
root <hvelez020@gmail.com>
2019-09-24 20:20:46 +0000

    [add] task #4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,8 +4,8 @@ const f = (error, response, code) => {
   if (error) { console.log(error); }
   let tmp; let cont = 0;
   for (const obj of JSON.parse(response.body).results) {
-    const url = 'https://swapi.co/api/people/18/';
-    tmp = obj.characters.filter(urlTest => url == urlTest);
+    const url = '/18/';
+    tmp = obj.characters.filter(urlTest => urlTest.includes(url));
     cont = cont + tmp.length;
   }
   console.log(cont);
