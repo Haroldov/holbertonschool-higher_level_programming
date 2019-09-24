6309253894b12fad129c44d9546572f4d3ebad65
root <hvelez020@gmail.com>
2019-09-24 20:37:46 +0000

    [add] task #6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const f = (error, response, code) => {
+  if (error) { console.log(error); }
+  const dict = {};
+  for (const obj of JSON.parse(response.body)) {
+    if (dict[obj.userId]) {
+      dict[obj.userId] = dict[obj.userId] + 1;
+    } else {
+      dict[obj.userId] = 1;
+    }
+  }
+  console.log(dict);
+};
+const url = process.argv[2];
+request(url, f);
