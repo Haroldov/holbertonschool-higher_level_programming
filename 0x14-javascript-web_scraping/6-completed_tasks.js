425c6b307723c728f9a0b60117559ec6ff0280df
root <hvelez020@gmail.com>
2019-09-24 20:42:11 +0000

    [add] else condition

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const f = (error, response, code) => {
-  if (error) { console.log(error); }
-  const dict = {};
-  for (const obj of JSON.parse(response.body)) {
-    if (dict[obj.userId]) {
-      dict[obj.userId] = dict[obj.userId] + 1;
-    } else {
-      dict[obj.userId] = 1;
+  if (error) { console.log(error); } else {
+    const dict = {};
+    for (const obj of JSON.parse(response.body)) {
+      if (dict[obj.userId]) {
+        dict[obj.userId] = dict[obj.userId] + 1;
+      } else {
+        dict[obj.userId] = 1;
+      }
     }
+    console.log(dict);
   }
-  console.log(dict);
 };
 const url = process.argv[2];
 request(url, f);
