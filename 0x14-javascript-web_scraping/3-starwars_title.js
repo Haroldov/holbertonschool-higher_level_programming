df7d94b4aeb5aeb6c2442e17a177a42311db7aa7
root <hvelez020@gmail.com>
2019-09-24 19:29:01 +0000

    [fix] semistandard tyle

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 const request = require('request');
 const f = (error, response, code) => {
   if (error) { console.log(error); }
-  console.log(JSON.parse(response.body).title)
+  console.log(JSON.parse(response.body).title);
 };
-let url = 'http://swapi.co/api/films/'
-url = url + process.argv[2]
+let url = 'http://swapi.co/api/films/';
+url = url + process.argv[2];
 request(url, f);
