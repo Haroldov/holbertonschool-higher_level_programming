69f3ed2a8b0433992b3fd00a30dfc0bb5ac45c1b
root <hvelez020@gmail.com>
2019-09-24 20:45:49 +0000

    [fix] count only when task are completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,10 +4,12 @@ const f = (error, response, code) => {
   if (error) { console.log(error); } else {
     const dict = {};
     for (const obj of JSON.parse(response.body)) {
-      if (dict[obj.userId]) {
-        dict[obj.userId] = dict[obj.userId] + 1;
-      } else {
-        dict[obj.userId] = 1;
+      if (obj.completed === true) {
+        if (dict[obj.userId]) {
+          dict[obj.userId] = dict[obj.userId] + 1;
+        } else {
+          dict[obj.userId] = 1;
+        }
       }
     }
     console.log(dict);
